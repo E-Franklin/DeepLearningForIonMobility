6ede549daa7ea06b43fb80d14d8026c80d0a1649
E-Franklin <emily.franklin@mail.utoronto.ca>
2021-06-02 14:02:10 -0400

    Backup

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+'''
 data = pd.read_csv('data\\2021-03-12-easypqp-frac-lib-openswath_new.tsv', sep='\t')
 
 # filter out the decoys
@@ -13,3 +14,17 @@ data.drop_duplicates(keep='first', inplace=True)
 data.reset_index(inplace=True, drop=True)
 
 data.to_csv('data\\2021-03-12-easypqp-frac-lib-openswath_processed.tsv', sep='\t', index=False)
+'''
+
+data = pd.read_csv('data\\SourceData_Figure_1.csv', sep=',')
+
+# count the number of modified peptides
+print(len(data))
+print(len(data[data['Modified sequence'] != data['Sequence']]))
+
+# create a no mods dataset from the CCS prediction one.
+# remove the _ from the start and end of each modified sequence
+data['Modified sequence'] = [i[1:-1] for i in data['Modified sequence']]
+
+# Take the rows where the modified and unmodified sequences match
+data = data[data['Modified sequence'] == data['Sequence']]
